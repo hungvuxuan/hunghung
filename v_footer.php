58104fe92d36968d4d540b8f38a0c9838c6616d6
hungvuxuan <hungvx42@wru.vn>
2017-05-14 19:47:14 +0700

    Add files via upload

---
+++ v_footer.php
@@ -0,0 +1,45 @@
+<div class="footer">
+	<div class="menu_footer">
+		<ul>
+			<li><a href="trang-chu.html">Trang Chủ</a></li>
+			<li><a href="san-pham-moi.html">Sản Phẩm Mới</a></li>
+			<li><a href="tin-tuc.html">Tin Tức</a></li>
+			<li><a href="gioi-thieu.html">Giới Thiệu</a></li>
+			<li><a href="lien-he.html">Liên Hệ</a></li>
+			<li><a href="ban-do.html">Bản Đồ</a></li>
+		</ul>
+	</div>
+	<div class="footer_logo"><a href="trang-chu.html"><img src="images\images_qc\23.jpg" height="150px;"></a></div>
+	<div class="footer_ht">
+		<div class="tt">
+			<ul>
+				<li style="font-weight:bold">Hỗ trợ khách hàng</li>
+				<br/>
+				<li><strong>Tel:</strong> 04.3999999</li>
+				<br/>
+				<li><strong>Hotline:</strong>  0969999999</li>
+				<br/>
+				<li><strong>Email:</strong>  lienhe@stylovelyshop.com.vn</li>
+				<br/>
+				<li><a href="#"><strong>Website:</strong>  www.styloveshop.com.vn</a></li>
+			</ul>
+		</div>
+		<div class="tt">
+			<ul>
+				<li style="font-weight:bold">Thông tin StylovelyShop</li>
+				<br/>
+				<li><a href="#">Tin tuyển dụng</a></li>
+				<br/>
+				<li><a href="#">Thông tin khuyến mại</a></li>
+				<br/>
+				<li><a href="#">Sơ đồ web</a></li>
+				<br/>
+				<li><a href="#">Thông tin về shop</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="copyright">
+		<p style="margin-top:5px; margin-bottom:5px">Bản quyền © 2017-2020 stylovelyshop.com.vn - Quần Áo Thời Trang !</p>
+		<p><strong>Số 175 Tây Sơn Đống Đa Hà Nội. Tel: 04-3999999</strong></p>
+	</div>       		
+</div>
---
+++ v_header.php
@@ -0,0 +1,32 @@
+
+	<div class="logo">  <!-- ảnh logo shop  -->
+		<div class="anh_logo"><a href="trang-chu.html"><img src="images\images_qc\24.jpg" style="height:90px; border-bottom-left-radius: 5px;"></a></div>
+		<!-- Địa chỉ shop -->
+		<div class="info_top">
+			<p style="margin-bottom:9px;">Stylovely Shop</p>
+			<p style="margin:4px;">Chuyên cung cấp Quần Áo cho giới trẻ</p>
+		</div>
+	</div>
+	<!-- Menu Top Sản phẩm -->
+	<div class="menu_top">
+		<ul>
+			<li><a href="trang-chu.html">Trang Chủ</a></li>
+			<li><a href="san-pham-moi.html">Sản Phẩm Mới</a></li>
+			<li><a href="tin-tuc.html">Tin Tức</a></li>
+			<li><a href="gioi-thieu.html">Giới Thiệu</a></li>
+			<li><a href="lien-he.html">Liên Hệ</a></li>
+			<li><a href="ban-do.html">Map</a></li>
+			<li><a href="huong-dan-mua-hang.html">Hotline : 0123456789</a></li>
+		</ul>
+	</div>
+	
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$(".zoom1").cycle({ 
+				fx:    'box', 
+				sync:  false, 
+				delay: -2000 
+			});
+		});
+	</script>
